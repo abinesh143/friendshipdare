e792e0e8723e3269a809ecc4c0c60a72e6996c81
abinesh143 <abi.posbyz@gmail.com>
2023-12-15 15:37:08 +0530

    friendship dare first commit

---
+++ src/app/page.js
@@ -0,0 +1,43 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import StartDare from "@/components/StartDare";
+import Modal from "@/components/Modal";
+import { useRouter } from "next/navigation";
+import Footer from "@/components/Footer";
+import Loader from "@/components/Loading";
+import Image from "next/image";
+
+export default function Home() {
+  const router = useRouter();
+  const [loader, setLoader] = useState(true)
+
+  useEffect(() => {
+    const quesExists = localStorage.getItem("quesExits");
+    const userExists = localStorage.getItem("quesUser");
+    if (quesExists && userExists) {
+      const user = JSON.parse(userExists);
+      router.replace(`/success/${user.id}`);
+    }
+    setLoader(false)
+  }, []);
+
+  useEffect(() => {
+    require("bootstrap/dist/js/bootstrap.bundle.min.js");
+  }, []);
+
+  return (
+    <main>
+      {loader ? (
+        <Loader />
+      ) : (
+        <div>
+          <StartDare />
+          <Image src='/animated.webp' alt="animated" height={300} width={300} />
+          <Footer />
+          <Modal></Modal>
+        </div>
+      )}
+    </main>
+  );
+}
